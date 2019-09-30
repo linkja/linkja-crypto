2ad442bbfef27596e3f196c3cfc3ad04ae60c388
Luke Rasmussen <luke.rasmussen@gmail.com>
2019-09-30 13:01:18 -0500

    Switching to cmake

--- prep-make.sh
+++ prep-make.sh
@@ -1,5 +1,1 @@
-aclocal # Set up an m4 environment
-autoconf # Generate configure from configure.ac
-automake --add-missing # Generate Makefile.in from Makefile.am
-./configure # Generate Makefile from Makefile.in
-make distcheck # Use Makefile to build and test a tarball to distribute
+cmake .
---
+++ src/CMakeFiles/liblinkjacrypto.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/liblinkjacrypto.dir/linkja-crypto.c.o"
+  "libliblinkjacrypto.dylib"
+  "libliblinkjacrypto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/liblinkjacrypto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
