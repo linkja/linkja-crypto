6db5ef455332e6b7456a85e2bde1e04eb5636821
Luke Rasmussen <luke.rasmussen@gmail.com>
2019-09-30 16:03:25 -0500

    Use local openssl libs

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ javac -h ./src/include ./src/java/Library.java
 
 # Compile our actual code
 mkdir -p out
-gcc -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin ./src/linkja-crypto.c -o ./out/linkja-crypto.o
+gcc -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin -I./lib/openssl/include ./src/linkja-crypto.c -o ./out/linkja-crypto.o
 
-# Create our bridge library
-gcc -dynamiclib -o ./out/liblinkjacrypto.dylib ./out/linkja-crypto.o -lc
+# Create our bridge library, and link in openssl
+gcc -dynamiclib ./out/linkja-crypto.o -L ./lib/openssl/macos -lcrypto -lc -o ./out/liblinkjacrypto.dylib
 
 # Compile our JAR
 mkdir -p out/java
--- src/linkja-crypto.c
+++ src/linkja-crypto.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include "include/linkja_crypto_Library.h"
 
+#include <openssl/aes.h>
+#include <openssl/rsa.h>
+
 JNIEXPORT void JNICALL Java_linkja_crypto_Library_test
   (JNIEnv* env, jobject thisObject) {
     printf("linkja-crypto test method\r\n");
