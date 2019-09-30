63b29a45ec38fbb48c7c814f165a69dc049d0468
Luke Rasmussen <luke.rasmussen@gmail.com>
2019-09-30 15:41:37 -0500

    WIP cmake

--- src/linkja-crypto.c
+++ src/linkja-crypto.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include "include/linkja_crypto_Library.h"
 
-//#include "openssl/include/aes.h"
+#include <openssl/rsa.h>
 
 JNIEXPORT void JNICALL Java_linkja_crypto_Library_test
   (JNIEnv* env, jobject thisObject) {
     printf("linkja-crypto test method\r\n");
+
+    RSA_free(NULL);
+    //AES_set_encrypt_key(NULL, NULL, NULL);
 }
