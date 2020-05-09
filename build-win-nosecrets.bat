85120562232401767f36c5b140961f16b3845b26
Luke Rasmussen <luke.rasmussen@gmail.com>
2020-05-08 22:50:59 -0500

    Windows build script

---
+++ build-win-nosecrets.bat
@@ -0,0 +1,11 @@
+del CMakeCache.txt
+del src\linkjacrypto.exp
+del src\linkjacrypto.lib
+del src\linkjacrypto.dll
+del src\linkjacrypto.dll.manifest
+
+REM del src\include\linkja_secret.h
+
+cmake -DCMAKE_BUILD_TYPE=Release -DINCLUDE_SECRETS=OFF -G "NMake Makefiles" .
+nmake clean
+nmake
