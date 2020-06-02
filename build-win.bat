751ecf3f0dc3052c6c662b118c4a343177aef8db
Luke Rasmussen <luke.rasmussen@gmail.com>
2020-06-02 15:17:45 -0500

    Add openssl path in Windows build

--- build-win.bat
+++ build-win.bat
@@ -6,6 +6,8 @@ del src\linkjacrypto.dll.manifest
 
 REM del src\include\linkja_secret.h
 
+PATH=%PATH%;C:\Development\OpenSSL\vc-win64a\bin
+
 cmake -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" .
 nmake clean
 nmake
